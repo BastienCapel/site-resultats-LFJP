487b92be72a17d593b97ebf0fbe92ce885a56288
Bastien Capel <capel.direction@gmail.com>
2026-06-27 11:08:58 +0000

    feat: populate DNB 2026 data from Google Sheet

--- src/exams/dnb.js
+++ src/exams/dnb.js
@@ -9,7 +9,36 @@ import { getMentionKey, MENTION_CONFIG } from "../data.js";
 
 export const ELEVES_DNB_2024 = [];
 export const ELEVES_DNB_2025 = [];
-export const ELEVES_DNB_2026 = [];
+export const ELEVES_DNB_2026 = [
+  { nom: "AUQUE", prenom: "Margaux Léane", score: 15.16, resultat: "Admis Mention Bien" },
+  { nom: "AUREJAC", prenom: "Nanou Fatou Rachel", score: 13.82, resultat: "Admis Mention Assez Bien" },
+  { nom: "BASSENE", prenom: "Maxence", score: 16.65, resultat: "Admis Mention Très Bien" },
+  { nom: "BEN SAID MASS", prenom: "Aicha Corinne", score: 10.22, resultat: "Admis" },
+  { nom: "CISSE", prenom: "Serigne Abdou Aziz", score: 14.72, resultat: "Admis Mention Bien" },
+  { nom: "CLAD", prenom: "Steffen", score: 10, resultat: "Admis" },
+  { nom: "DEBOES", prenom: "Iman", score: 14.3, resultat: "Admis Mention Bien" },
+  { nom: "DERVAL", prenom: "Antoine Julien", score: 14.9, resultat: "Admis Mention Bien" },
+  { nom: "DESCHUYTER", prenom: "Alicia", score: 18.05, resultat: "Admis Mention Très Bien" },
+  { nom: "DHENIN", prenom: "Chloé", score: 14.22, resultat: "Admis Mention Bien" },
+  { nom: "DIA", prenom: "Gwendoline Aïcha", score: 10.31, resultat: "Admis" },
+  { nom: "DIA", prenom: "Mamadou", score: 11.5, resultat: "Admis" },
+  { nom: "DIEYE", prenom: "Alyssa", score: 15.26, resultat: "Admis Mention Bien" },
+  { nom: "GARA", prenom: "Aïda", score: 17.2, resultat: "Admis Mention Très Bien" },
+  { nom: "GASSAMA", prenom: "Dieynaba", score: 14.16, resultat: "Admis Mention Bien" },
+  { nom: "KANDJI", prenom: "Elhadj Mactar", score: 12.95, resultat: "Admis Mention Assez Bien" },
+  { nom: "MBENGUE", prenom: "Ndeye Saly", score: 8.65, resultat: "Refusé" },
+  { nom: "MECHALLAL", prenom: "Anissa", score: 15.55, resultat: "Admis Mention Bien" },
+  { nom: "NDIAYE", prenom: "Mouhamed Bachir", score: 15.94, resultat: "Admis Mention Bien" },
+  { nom: "NIOX", prenom: "Amary René", score: 12.09, resultat: "Admis Mention Assez Bien" },
+  { nom: "ONCINA", prenom: "Iyan Tidiane Manuel", score: 15.56, resultat: "Admis Mention Bien" },
+  { nom: "PAILLARD", prenom: "Thomas René Thierry", score: 9, resultat: "Absent" },
+  { nom: "RASHID", prenom: "Lina Sonia", score: 15.3, resultat: "Admis Mention Bien" },
+  { nom: "SENE", prenom: "Lena", score: 12.31, resultat: "Admis Mention Assez Bien" },
+  { nom: "SENE", prenom: "Moussa", score: 12.99, resultat: "Admis Mention Assez Bien" },
+  { nom: "SY", prenom: "Seynabou Babacar", score: 16.9, resultat: "Admis Mention Très Bien" },
+  { nom: "TEBER", prenom: "Nihat", score: 12.04, resultat: "Admis Mention Assez Bien" },
+  { nom: "TOURE", prenom: "Dahina", score: 12.11, resultat: "Admis Mention Assez Bien" }
+];
 
 export const dnbExam = {
   id: "dnb",
